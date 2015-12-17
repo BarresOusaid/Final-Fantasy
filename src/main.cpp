80641d96fb8211db50eeea9eaab5231e8e9e80bc
BarresOusaid <sandochebarres@hotmail.fr>
2015-12-17 22:00:42 +0100

    tag 3.Final

--- src/main.cpp
+++ src/main.cpp
@@ -21,15 +21,27 @@
 #include "Player.hpp"
 #include "TileMap.hpp"
 #include "Map.hpp"
-#include "Grid.hpp"
 #include "Input.hpp"
 #include "CollisionManager.hpp"
-#include "Services.hpp"
+#include <thread>
 
 using namespace sf;
 using namespace std;
 
-class Grid;
+
+Event event;
+Player *player = new Player();
+Input *input = new Input();
+Map *map_1 = new Map();
+
+void character_movement(void)
+{
+    input->pollInput(event);
+    if(CollisionManager::collidesWithMap(map_1))
+    {
+        player->moveCharacterSprite(input->getDirection());
+    }
+}
 
 int main(int, char const**)
 {
@@ -39,19 +51,21 @@ int main(int, char const**)
     Time time;
     
     Clock clock;
-    Player *player = new Player();
+    //Player *player = new Player();
     Player *second = new Player(2,2,"Hero");
     
-    Input *input = new Input();
-
-    Map *map = new Map();
-
+    //Input *input = new Input();
+    /*thread t(character_movement);
+    t.join();*/
+    
+    //Map *map = new Map();
+    /*View view(sf::FloatRect(0, 0, 1000, 800));
+    window.setView(view);*/
     while (window.isOpen())
     {	
-        map->init(player, input);
-        map->popMap(1, map);
+        map_1->init(player, input);
+        map_1->popMap(1, map_1);
         time = clock.getElapsedTime();
-
         int xPos, yPos;
         xPos=player->getTileMap().getPosition().x;
         yPos=player->getTileMap().getPosition().y;
@@ -59,35 +73,41 @@ int main(int, char const**)
         if(CollisionManager::collidesWithPlayer(player, second)==1)
         {
 			second->moveCharacterSprite_auto(1,2,2);
-        } 
+        }
         
-		if(((!CollisionManager::collidesWithMap(map))))
+		if(((!CollisionManager::collidesWithMap(map_1))))
         {
-			player->moveCharacterSprite_auto(2, 1, 1);		
+            player->moveCharacterSprite_auto(2, 1, 1);
         }      
-       //second -> moveCharacterSprite_IA(map);
+        //second -> moveCharacterSprite_IA(map_1);
+        /*if(((!CollisionManager::collidesWithMap(map))&&(input->downPressed()==0)))
+        {
+			player->moveCharacterSprite_auto(1, 1, 1);
+        }*/   
               
         // Process events
-        sf::Event event;
+        //sf::Event event;
         while (window.pollEvent(event))
         {
-            input->pollInput(event);
-            if(CollisionManager::collidesWithMap(map))
+            /*input->pollInput(event);
+            if(CollisionManager::collidesWithMap(map_1))
             {
                 player->moveCharacterSprite(input->getDirection());
-            }
+            }*/
             
             if(event.type == Event::Closed)
             {
                 window.close();
-            }	
+            }
+            thread t(character_movement);
+            t.join();
         }
         
         // Clear screen
         window.clear();
         
         // Draw the sprite
-        window.draw(map->getMap());
+        window.draw(map_1->getMap());
         
         // Draw the string
         window.draw(player->getTileMap());
