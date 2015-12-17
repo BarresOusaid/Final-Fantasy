72c99017579eaad1d13167e74f80035ac480f507
BarresOusaid <sandochebarres@hotmail.fr>
2015-12-17 22:15:26 +0100

    tag 3.Final

--- src/Options.h
+++
@@ -1,9 +0,0 @@
-#ifndef OPTIONS__H
-#define OPTIONS__H
-
-enum Options {
-  NEW,
-  CONTINUE
-};
-
-#endif
--- src/TewInfo.h
+++
@@ -1,9 +0,0 @@
-#ifndef TEWINFO__H
-#define TEWINFO__H
-
-enum TewInfo {
-  LOGO,
-  COUNT
-};
-
-#endif
